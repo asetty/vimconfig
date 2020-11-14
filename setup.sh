6158f836a6d77f09b6d07651f405b591f13b9245
asetty <asetters7@gmail.com>
2020-11-14 18:20:49 -0500

    add conditional for install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install homebrew
+if ! command -v brew &> /dev/null; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# install vundle
+if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 brew install tmux
 brew install golang
 brew install kubectl
